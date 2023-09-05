7a4fe10d9a4b23fb266c937eaaee847a17f3bba1
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-05 10:16:57 +0300

    Client <> Created heeader component

---
+++ src/components/Shared/Header/index.jsx
@@ -0,0 +1,12 @@
+import '../Header/style.css';
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <div className='Header'> 
+            LOGED IN <span>{props.name}</span>
+        </div>
+    );
+}
+
+export default Header;
