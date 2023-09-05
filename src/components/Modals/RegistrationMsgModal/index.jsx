99d2173847d1033d6cf446e1035cf5acb7a99414
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-05 10:29:51 +0300

    Client <> Created register message modal

---
+++ src/components/Modals/RegistrationMsgModal/index.jsx
@@ -0,0 +1,21 @@
+import '../RegistrationMsgModal/style.css';
+import Modal from 'react-modal';
+import React from "react";
+
+const RegistrationMsgModal = ({ openModal, handleCloseModal }) => {
+    return (
+        <div>
+            <Modal isOpen={openModal} className="registrationModal">
+                <div className='body'>
+                    <div className='text'>
+                    Thank you for placing your trust in us. We will review your registration and send your acceptance via email.
+                    </div>
+                    <button onClick={handleCloseModal}>Close</button>
+                </div>
+            </Modal>
+            
+        </div>
+    );
+}
+
+export default RegistrationMsgModal;
