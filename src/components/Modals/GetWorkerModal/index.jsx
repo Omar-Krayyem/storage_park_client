f075c0d21865cc0c5ca74634930c7da54811141a
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-14 22:03:41 +0300

    Client <> updated design of get worker modal for admin

--- src/components/Modals/GetWorkerModal/index.jsx
+++ src/components/Modals/GetWorkerModal/index.jsx
@@ -2,6 +2,7 @@ import './style.css';
 import Modal from 'react-modal';
 import React, { useState, useEffect }from "react";
 import axios from 'axios';
+import {AiOutlineClose} from 'react-icons/ai'
 
 const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
     const [first_name, setFName] = useState("");
@@ -9,7 +10,8 @@ const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
     const [email, setEmail] = useState("");
     const [address, setAddress] = useState("");
     const [phone, setPhone] = useState("");
-
+    const [error, setError] = useState("");
+    const [msg, setMsg] = useState("");
 
     const token = localStorage.getItem("token");
 
@@ -53,6 +55,12 @@ const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
     const updateUser = (e) => {
         e.preventDefault();
 
+        if (!/^\d+$/.test(phone)) {
+            setError("Phone number should contain only numbers");
+            setTimeout(() => setError(""), 2000);
+            return;
+        }
+
         const postData = {user_id, first_name, last_name, email, phone, address};
         console.log(postData)
     
@@ -63,13 +71,17 @@ const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
         })
         .then(response => {
             console.log(response.data.data);
-            // window.location.reload();
+            setMsg("Updated Successfullyy")
+            setTimeout(() => setMsg(""), 2000);
         })
         .catch(error => {
-            console.log(error);
+            if (error.response && error.response.data && error.response.data.data) {
+                setError(error.response.data.data);
+            } else {
+                setError("An error occurred");
+            }
+            setTimeout(() => setError(""), 2000);
         });
-
-
     }
 
     useEffect(() => {
@@ -79,10 +91,20 @@ const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
 
     return (
         <div>
-            <Modal isOpen={openModal} className="partnerModal">
+            <Modal isOpen={openModal} className="WorkerModal">
                 <div className='body'>
+                <div className='title'>
+                    <h1>Worker</h1>
+                    <AiOutlineClose 
+                    onClick={async (e) => {
+                        e.preventDefault();
+                        handleCloseModal();
+                    }}
+                    className='icon'
+                    size={25}/>
+                </div>
                 <div className="form_body">
-                        <form className='partnerForm'>
+                        <form className='workerForm plus'>
                             <div className='nameSection'>
                                 <div className="halftext_feild">
                                     <label>First Name</label>
@@ -136,19 +158,12 @@ const GetWorkerModal = ({ openModal, handleCloseModal, user_id }) => {
                                 onChange={(e) => setAddress(e.target.value)}
                                 ></input> 
                             </div>
+                            <div className='error'>{error}</div>
+                            <div className='msg'>{msg}</div>
                             <div className='btnSection'>
                                 <button className='btn' onClick={updateUser}>Update</button>
                                 <button className='btn' onClick={deleteUser}>Delete</button>
                             </div>
-                            <button
-                                type="submit"
-                                className="close btn"
-                                value="Close"
-                                onClick={async (e) => {
-                                    e.preventDefault();
-                                    handleCloseModal();
-                                }}
-                            >Close</button>
                         </form>
                     </div>
                 </div>
