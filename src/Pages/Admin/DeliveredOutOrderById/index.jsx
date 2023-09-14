2a30d3e2ea8ab875e5e483d94484ec01ab73c026
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-15 00:30:03 +0300

    Client <> updated design of deivered outgoing orders by id page for admin

--- src/Pages/Admin/DeliveredOutOrderById/index.jsx
+++ src/Pages/Admin/DeliveredOutOrderById/index.jsx
@@ -54,110 +54,34 @@ const AdminOutgoingDeliveredOrder = () => {
                         </div>
                     </div>
 
-                    <div className='Order_Info'>
-                        
-                        <div className='info_row'>
-                            <h2>Order Info:</h2>
-                            <div className="halftext_feild">
-                                <label>Company Name</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={order.user?.company_name || ''}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild">
-                                <label>Total Price</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.total_price}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Placed At</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.placed_at}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Delivered At</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.delivered_at}
-                                    disabled
-                                ></input>
-                            </div>
-                        </div>
-
-                        <div className='info_row'>
-                            <h2>Customer Info:</h2>
-                            <div className="halftext_feild">
-                                <label>Name</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.customer?.name}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild">
-                                <label>Email</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={order.customer?.email}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Phone</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.customer?.phone}
-                                    disabled
-                                ></input>
-                            </div>
-                        </div>
-
-                        <div className='info_row'>
-                            <h2>Location Info:</h2>
-                            <div className="halftext_feild">
-                                <label>Latitude</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={latitude}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Longitude</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={longitude}
-                                    disabled
-                                ></input>
-                            </div>
-                        </div>
-                        <div className='mapContainer'>
+                    <div className='order_detail_table'>
+                        <table className='AdminOutgoingDeliveredOrder_table'>
+                            <thead className='AdminOutgoingDeliveredOrder_thead'>
+                                <tr className=''>
+                                    <th className='AdminOutgoingDeliveredOrder_th top_left'>Company Name</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Customer Name</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Customer Email</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Customer Phone</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Total Price $</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Placed At</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th top_right'>Delivered At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className='AdminOutgoingDeliveredOrder_tr'>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.user?.company_name || ''}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.customer?.name}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.customer?.email}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.customer?.phone}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.total_price}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.placed_at}</td>
+                                    <td className='AdminOutgoingDeliveredOrder_td'>{order.delivered_at}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                  
+                    <div className='mapContainer'>
                         {mapDataLoaded && (
                             <div className='mapContainer'>
                                 <Map
@@ -169,30 +93,39 @@ const AdminOutgoingDeliveredOrder = () => {
                                 </Map>
                             </div>
                         )}
-                        </div>
                     </div>
 
-                    <div className='products_section'>
+                    <div className='order_table'>
                         <h2>Order Items:</h2>
-                        <div className='display_products'>
-                            {orderItems.map((item, index) => (
-                                <div className='display_product' key={index}>
-                                    <div className='attributes'>
-                                        <div><span>Name: </span>{item.product.name}</div>
-                                        <div><span>Description: </span>{item.product.description}</div>
-                                        <div><span>Category: </span>{item.product.category.category}</div>
-                                        <div><span>Price: </span>{item.product.price}$</div>
-                                        <div><span>Quantity: </span>{item.quantity}</div>
-                                    </div>
-                                </div>
-                            ))}         
-                        </div>
+                        <table className='AdminOutgoingDeliveredOrder_table'>
+                            <thead className='AdminOutgoingDeliveredOrder_thead'>
+                                <tr className=''>
+                                    <th className='AdminOutgoingDeliveredOrder_th top_left'>Name</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Descritpion</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Category</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th'>Price $</th>
+                                    <th className='AdminOutgoingDeliveredOrder_th top_right'>Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderItems.map((item, index) => (
+                                    <tr className='AdminOutgoingDeliveredOrder_tr'>
+                                        <td className='AdminOutgoingDeliveredOrder_td'>{item.product.name}</td>
+                                        <td className='AdminOutgoingDeliveredOrder_td'>{item.product.description}</td>
+                                        <td className='AdminOutgoingDeliveredOrder_td'>{item.product.category.category}</td>
+                                        <td className='AdminOutgoingDeliveredOrder_td'>{item.product.price}</td>
+                                        <td className='AdminOutgoingDeliveredOrder_td'>{item.quantity}</td>
+                                    </tr>
+                                ))} 
+                            </tbody>
+                        </table>
                     </div>
+
                     <div>
                         <div className="halftext_feild ">
-                            <label>Worker</label>
+                            <label>Selected Worker</label>
                             <input
-                                className='half'
+                                className='creatableSelect'
                                 type="text"
                                 required
                                 value={worker}
