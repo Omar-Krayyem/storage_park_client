41632a4f2495b17016ef2c754ec787de746d1b4a
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-15 15:24:34 +0300

    Client <> fetched get statics api for partner dashboard page

--- src/Pages/Partner/Dashboard/index.jsx
+++ src/Pages/Partner/Dashboard/index.jsx
@@ -6,9 +6,9 @@ import Barchart from '../../../components/Admin/Barchart';
 
 const Dashboard = () => {
 
-    const [ requests , setRequests] = useState(0);
-    const [ partners , setPartners] = useState(0);
-    const [ workers , setWorkers] = useState(0);
+    const [ orders , setOrders] = useState(0);
+    const [ products , setProducts] = useState(0);
+    const [ items , setItems] = useState(0);
 
     const [ placedInc , setPlacedInc] = useState(0);
     const [ shipmentInc , setShipmentInc] = useState(0);
@@ -17,7 +17,41 @@ const Dashboard = () => {
     const [ placedOut , setPlacedOut] = useState(0);
     const [ shipmentOut , setShipmentOut] = useState(0);
     const [ deliveredOut , setDeliveredOut] = useState(0);
-    
+
+    const token = localStorage.getItem("token");
+
+        const getUser = async () => {
+            await axios.get(`http://127.0.0.1:8000/api/partner/dashboard`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                console.log(response.data.data)
+                setOrders(response.data.data.order_count)
+                setProducts(response.data.data.product_count)
+                setItems(response.data.data.item_count)
+
+                setPlacedInc(response.data.data.placedInc)
+                setShipmentInc(response.data.data.shipmentInc)
+                setDeliveredInc(response.data.data.deliveredInc)
+
+                setPlacedOut(response.data.data.placedOut)
+                setShipmentOut(response.data.data.shipmentOut)
+                setDeliveredOut(response.data.data.deliveredOut)
+
+                setCategories(response.data.data.category)
+            })
+            .catch(error => {
+                console.log(error);
+            });    
+        };
+
+        useEffect(() => {
+            getUser();
+        }, []);
+
+
     const [categories, setCategories] = useState([]);
     const BarData = [
         {
@@ -50,13 +84,13 @@ const Dashboard = () => {
                 <div className='title'><h1>Dashboard</h1></div>
                     <div className='records'>
                         <div className='record'>
-                            <span>{requests}</span>Requests
+                            <span>{orders}</span>Orders
                         </div>
                         <div className='record'>
-                            <span>{partners}</span>Partners
+                            <span>{products}</span>Products
                         </div>
                         <div className='record'>
-                            <span>{workers}</span>Workers
+                            <span>{items}</span>Items
                         </div>
                     </div>
                     <div className='graphs'>
