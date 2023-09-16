892515a51392af7fc8fd682a0abaf4ca4cef174a
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-16 12:23:21 +0300

    Client <> updated design of incoming delivered order by id page for worker

--- src/Pages/Worker/DeliveredOrderById/index.jsx
+++ src/Pages/Worker/DeliveredOrderById/index.jsx
@@ -40,7 +40,7 @@ const DeliveredOrderById = () => {
 
 
     return (
-        <div className='AddOrder_page'>
+        <div className='WorkerIncomingDeliveredOrder_page'>
                 <div className='body'>
                     <div className='title'>
                         <div className='page_title'><h1>Placed Order ID: {id}</h1></div>
@@ -49,76 +49,29 @@ const DeliveredOrderById = () => {
                         </div>
                     </div>
 
-                    <div className='location_section'>
-                        
-                        <div className='location_inputs'>
-                            <h2>Order Info:</h2>
-                            <div className="halftext_feild">
-                                <label>Company Name</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.user?.company_name || ''}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild">
-                                <label>Total Price</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.total_price}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Placed At</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.placed_at}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Delivered At</label>
-                                <input
-                                    className='Q'
-                                    type="text"
-                                    required
-                                    value={order.delivered_at}
-                                    disabled
-                                ></input>
-                            </div>
-                        </div>
+                    <div className='order_table'>
+                        <table className='WorkerIncomingDeliveredOrder_table'>
+                            <thead className='WorkerIncomingDeliveredOrder_thead'>
+                                <tr className=''>
+                                    <th className='WorkerIncomingDeliveredOrder_th top_left'>Company Name</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th'>Total Price $</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th'>Placed At</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th top_right'>Delivered At</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className='WorkerIncomingDeliveredOrder_tr'>
+                                    <td className='WorkerIncomingDeliveredOrder_td'>{order.user?.company_name || ''}</td>
+                                    <td className='WorkerIncomingDeliveredOrder_td'>{order.total_price}</td>
+                                    <td className='WorkerIncomingDeliveredOrder_td'>{order.placed_at}</td>
+                                    <td className='WorkerIncomingDeliveredOrder_td'>{order.delivered_at}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
 
-                        <div className='location_inputs'>
-                            <h2>Location Info:</h2>
-                            <div className="halftext_feild">
-                                <label>Latitude</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={latitude}
-                                    disabled
-                                ></input>
-                            </div>
-                            <div className="halftext_feild ">
-                                <label>Longitude</label>
-                                <input
-                                    className='half'
-                                    type="text"
-                                    required
-                                    value={longitude}
-                                    disabled
-                                ></input>
-                            </div>
-                        </div>
-                        <div className='mapContainer'>
+                        
+                    <div className='mapContainer'>
                         {mapDataLoaded && (
                             <div className='mapContainer'>
                                 <Map
@@ -130,25 +83,34 @@ const DeliveredOrderById = () => {
                                 </Map>
                             </div>
                         )}
-                        </div>
                     </div>
 
-                    <div className='products_section'>
+                    <div className='order_table'>
                         <h2>Order Items:</h2>
-                        <div className='display_products'>
-                            {orderItems.map((item, index) => (
-                                <div className='display_product' key={index}>
-                                    <div className='attributes'>
-                                        <div><span>Name: </span>{item.product.name}</div>
-                                        <div><span>Description: </span>{item.product.description}</div>
-                                        <div><span>Category: </span>{item.product.category.category}</div>
-                                        <div><span>Price: </span>{item.product.price}$</div>
-                                        <div><span>Quantity: </span>{item.quantity}</div>
-                                    </div>
-                                </div>
-                            ))}         
-                        </div>
+                        <table className='WorkerIncomingDeliveredOrder_table'>
+                            <thead className='WorkerIncomingDeliveredOrder_thead'>
+                                <tr className=''>
+                                    <th className='WorkerIncomingDeliveredOrder_th top_left'>Name</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th'>Descritpion</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th'>Category</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th'>Price $</th>
+                                    <th className='WorkerIncomingDeliveredOrder_th top_right'>Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderItems.map((item, index) => (
+                                    <tr className='WorkerIncomingDeliveredOrder_tr'>
+                                        <td className='WorkerIncomingDeliveredOrder_td'>{item.product.name}</td>
+                                        <td className='WorkerIncomingDeliveredOrder_td'>{item.product.description}</td>
+                                        <td className='WorkerIncomingDeliveredOrder_td'>{item.product.category.category}</td>
+                                        <td className='WorkerIncomingDeliveredOrder_td'>{item.product.price}</td>
+                                        <td className='WorkerIncomingDeliveredOrder_td'>{item.quantity}</td>
+                                    </tr>
+                                ))} 
+                            </tbody>
+                        </table>
                     </div>
+
                 </div>
         </div>
     );
