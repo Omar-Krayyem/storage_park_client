e93d329d1abf63a2c20f3e711cb73dfcce7ab394
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-17 14:13:17 +0300

    Client <> logo clickable in login page

--- src/Pages/Auth/Login/index.jsx
+++ src/Pages/Auth/Login/index.jsx
@@ -54,7 +54,7 @@ const Login = () => {
         <div className='LoginPage'>
             <div className='loginComponent'>
                 <div className='leftSide'>
-                    <img src={logo} alt="Logo"></img>
+                <Link to='/'><img  src={logo} alt="Logo"></img></Link>
                 </div>
                 <div className='rightSide'>
                     <div className="form_header">
