53ec10327b8330c275fb896ff3145471be6b5497
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-20 11:33:01 +0300

    Client <> create track order modal

---
+++ src/components/Modals/OrderTrackModal/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+import Modal from 'react-modal';
+import React from "react";
+
+const OrderTrackModal = ({ openModal, handleCloseModal }) => {
+    return (
+        <div>
+            <Modal isOpen={openModal} className="registrationModal">
+                
+            </Modal>
+        </div>
+    );
+}
+
+export default OrderTrackModal;
