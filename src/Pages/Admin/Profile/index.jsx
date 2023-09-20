90b28de2cac6477e90498477b1d3897e9ce1e82e
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-20 16:35:56 +0300

    Client <> updated the profile page for admin

--- src/Pages/Admin/Profile/index.jsx
+++ src/Pages/Admin/Profile/index.jsx
@@ -1,6 +1,7 @@
 import './style.css';
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import PasswordModal from '../../../components/Modals/PasswordModal';
 
 const Profile = () => {
     const [user_id, setId] = useState(0);
@@ -9,12 +10,20 @@ const Profile = () => {
     const [email, setEmail] = useState("");
     const [address, setAddress] = useState("");
     const [phone, setPhone] = useState("");
-    const [password, setPassword] = useState("");
-    const [conPassword, setConPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
     const [successMessage, setSuccessMessage] = useState("");
 
     const token = localStorage.getItem("token");
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    }
 
     const getUser = async () => {
         await axios.get(`http://127.0.0.1:8000/api/admin/profile/get`, {
@@ -45,25 +54,7 @@ const Profile = () => {
             return;
         }
 
-        if((password !== "" && conPassword === "") ||(password === "" && conPassword !== "")){
-            setErrorMessage("To change password fill the all password fields");
-            setTimeout(() => setErrorMessage(""), 3000);
-            return;
-        }
-
-        if(password.length < 6){
-            setErrorMessage("Password is too short");
-            setTimeout(() => setErrorMessage(""), 3000);
-            return;
-        }
-
-        if ((password && conPassword) && (password !== conPassword || password.length < 6)) {
-            setErrorMessage("Passwords do not match");
-            setTimeout(() => setErrorMessage(""), 3000);
-            return;
-        }
-
-        const postData = {user_id, first_name, last_name, email, phone, address, password};
+        const postData = {user_id, first_name, last_name, email, phone, address};
 
         axios.post('http://127.0.0.1:8000/api/admin/profile', postData, {
             headers: {
@@ -145,32 +136,17 @@ const Profile = () => {
                                     value={address}
                                 ></input> 
                             </div>
-                            <div className="text_feild">
-                                <label>Password</label>
-                                <input 
-                                    className='full'
-                                    type="password"
-                                    required
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                ></input> 
-                            </div>
-                            <div className="text_feild">
-                                <label>Confirm Password</label>
-                                <input 
-                                    className='full'
-                                    type="password"
-                                    required
-                                    onChange={(e) => setConPassword(e.target.value)}
-                                    value={conPassword}
-                                ></input> 
-                            </div>
-                            <div className='error'>{errorMessage}</div>
-                            <div className='success'>{successMessage}</div>
+                            {errorMessage && <div className='error'>{errorMessage}</div>}
+                            {successMessage && <div className='success'>{successMessage}</div>}
+                            <button className='btn' onClick={handleOpenModal}>Change Password</button>
                             <button className='btn' onClick={updateInfo}>Update</button>
                         </form>
                     </div>
                 </div>
+                <PasswordModal
+                    handleCloseModal={handleCloseModal}
+                    openModal={openModal} 
+                />
             </div>        
     );
 }
--- src/components/Modals/PasswordModal/index.jsx
+++ src/components/Modals/PasswordModal/index.jsx
@@ -1,13 +1,64 @@
 import './style.css';
 import Modal from 'react-modal';
 import React, {useState} from "react";
-import {AiOutlineClose} from 'react-icons/ai'
+import {AiOutlineClose} from 'react-icons/ai';
+import axios from 'axios';
 
 const PasswordModal = ({ openModal, handleCloseModal }) => {
-    const [order, setOrder] = useState("");
+    const [password, setPassword] = useState("");
+    const [conPassword, setConPassword] = useState(""); 
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const token = localStorage.getItem("token");
+
+    const updatePassword = (e) => {
+        e.preventDefault();
+
+        if((password !== "" && conPassword === "") ||(password === "" && conPassword !== "")){
+            setErrorMessage("To change password fill the all password fields");
+            setTimeout(() => setErrorMessage(""), 3000);
+            return;
+        }
+
+        if(password.length < 6){
+            setErrorMessage("Password is too short");
+            setTimeout(() => setErrorMessage(""), 3000);
+            return;
+        }
+
+        if ((password && conPassword) && (password !== conPassword || password.length < 6)) {
+            setErrorMessage("Passwords do not match");
+            setTimeout(() => setErrorMessage(""), 3000);
+            return;
+        }
+
+        const postData = {password};
+
+        axios.post('http://127.0.0.1:8000/api/admin/password', postData, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            console.log(response.data.data);
+            setSuccessMessage("Updated Successfully");
+            setTimeout(() => setSuccessMessage(""), 3000);
+            
+        })
+
+        .catch(error => {
+            if (error.response && error.response.data && error.response.data.data) {
+                setErrorMessage(error.response.data.data);
+            } else {
+                setErrorMessage("An error occurred");
+            }
+            setTimeout(() => setErrorMessage(""), 3000);
+        });
+    }
+    
     return (
-        <div>
-            <Modal isOpen={openModal} className="PasswordModal">
+        <Modal isOpen={openModal} className="PasswordModal" >
             <div className='title'>
                         <h1>Upfate Password</h1>
                         <AiOutlineClose 
@@ -17,16 +68,16 @@ const PasswordModal = ({ openModal, handleCloseModal }) => {
                         }}
                         className='icon'
                         size={25}/>
-                    </div>
-                <div className='body'>
+            </div>
+            <div className='body'>
                 <div className="text_feild">
                         <label>Password</label>
                         <input 
                             className='full'
                             type="text" 
                             required
-                            value={order}
-                            onChange={(e) => setOrder(e.target.value)}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         ></input>
                     </div>
                     <div className="text_feild">
@@ -35,14 +86,16 @@ const PasswordModal = ({ openModal, handleCloseModal }) => {
                             className='full'
                             type="text" 
                             required
-                            value={order}
-                            onChange={(e) => setOrder(e.target.value)}
+                            value={conPassword}
+                            onChange={(e) => setConPassword(e.target.value)}
                         ></input>
                     </div>
-                    <button>Track Order</button>
-                </div>
-            </Modal>
-        </div>
+                    
+                    {errorMessage && <div className='error'>{errorMessage}</div>}
+                    {successMessage && <div className='success'>{successMessage}</div>}
+                    <button onClick={updatePassword}>Update</button>
+            </div>
+        </Modal>
     );
 }
 
