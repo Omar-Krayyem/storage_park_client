382c1c993934467a3ed7aca08ab45869fd7ef44d
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-20 11:58:23 +0300

    Client <> designed track order modal

--- src/Pages/Landing/index.jsx
+++ src/Pages/Landing/index.jsx
@@ -9,7 +9,18 @@ import icon2 from '../../images/sol2.png';
 import icon3 from '../../images/sol3.png';
 import icon4 from '../../images/sol4.png';
 import icon5 from '../../images//sol5.png';
+import OrderTrackModal from '../../components/Modals/OrderTrackModal';
 const Landing = () => {
+
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    }
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    }
 
     return (
         <div className='Landing_page'> 
@@ -22,7 +33,7 @@ const Landing = () => {
                         storage park couples powerful software with its flexible nationwide fulfillment network, enabling brands to offer consistently efficient, 
                         cost-effective delivery that improves the customer experience, from every channel.
                         </p>
-                        <button>Track Your Order</button>
+                        <button onClick={handleOpenModal}>Track Your Order</button>
                     </div>
                     <div><img src={h1} alt='hero image'></img></div>
                 </div>
@@ -84,6 +95,10 @@ const Landing = () => {
                 <div className='section hero3'><img src={h2} alt='image'></img></div>
             </div>
             <div className="footer"><Footer/></div>
+            <OrderTrackModal
+            handleCloseModal={handleCloseModal}
+            openModal={openModal}
+            />
         </div>
     );
 }
--- src/components/Modals/OrderTrackModal/index.jsx
+++ src/components/Modals/OrderTrackModal/index.jsx
@@ -1,12 +1,37 @@
 import './style.css';
 import Modal from 'react-modal';
-import React from "react";
+import React, {useState} from "react";
+import {AiOutlineClose} from 'react-icons/ai'
 
 const OrderTrackModal = ({ openModal, handleCloseModal }) => {
+    const [order, setOrder] = useState("");
     return (
         <div>
-            <Modal isOpen={openModal} className="registrationModal">
-                
+            <Modal isOpen={openModal} className="orderModal">
+                <div className='body'>
+                    <div className='title'>
+                        <h1>Track Your Order</h1>
+                        <AiOutlineClose 
+                        onClick={async (e) => {
+                            e.preventDefault();
+                            handleCloseModal();
+                        }}
+                        className='icon'
+                        size={25}/>
+                    </div>
+                    
+                    <div className="text_feild">
+                        <label>Order ID</label>
+                        <input 
+                            className='full'
+                            type="text" 
+                            required
+                            value={order}
+                            onChange={(e) => setOrder(e.target.value)}
+                        ></input>
+                    </div>
+                    <button>Track Order</button>
+                </div>
             </Modal>
         </div>
     );
