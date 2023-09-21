191a54a145f7bb9390a6fd6820c0e262523b57e0
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-09-21 11:12:51 +0300

    Client <> used redux in header component

--- src/components/Shared/Header/index.jsx
+++ src/components/Shared/Header/index.jsx
@@ -1,8 +1,9 @@
 import './style.css';
 import React from "react";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
-    const name = localStorage.getItem("user_name");
+    const name = useSelector((store) => store.user.user_name);
     return (
         <div className='Header'> 
             LOGGED IN <span>{name}</span>
